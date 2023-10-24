059dee6bf48d486dff631b69a754703aac6608c4
Siviwe <60976457+Svi-g@users.noreply.github.com>
2023-10-24 19:16:20 +0200

    May the force be with you!

--- src/main/java/com/example/thejedifxpack/MenuDemo.java
+++ src/main/java/com/example/thejedifxpack/MenuDemo.java
@@ -2,11 +2,13 @@ package com.example.thejedifxpack;
 // Demonstrate Menus
 import javafx.application.*;
 import javafx.scene.*;
+import javafx.scene.image.ImageView;
 import javafx.stage.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.event.*;
-import javafx.geometry.*;
+//import javafx.geometry.*;
+import javafx.scene.input.*;
 
 
 public class MenuDemo extends Application {
@@ -30,7 +32,7 @@ public class MenuDemo extends Application {
 // Create the menu bar.
         MenuBar mb = new MenuBar();
 // Create the File menu.
-        Menu fileMenu = new Menu("File");
+        Menu fileMenu = new Menu("_File");
         MenuItem open = new MenuItem("Open");
         MenuItem close = new MenuItem("Close");
         MenuItem save = new MenuItem("Save");
@@ -41,8 +43,47 @@ public class MenuDemo extends Application {
         mb.getMenus().add(fileMenu);
 // Create the Options menu.
         Menu optionsMenu = new Menu("Options");
-// Create the Colors submenu.
+        // Create the Colors submenu.
         Menu colorsMenu = new Menu("Colors");
+// Use check menu items for colors. This allows
+// the user to select more than one color.
+        CheckMenuItem red = new CheckMenuItem("Red");
+        CheckMenuItem green = new CheckMenuItem("Green");
+        CheckMenuItem blue = new CheckMenuItem("Blue");
+        colorsMenu.getItems().addAll(red, green, blue);
+        optionsMenu.getItems().add(colorsMenu);
+// Select green for the default color selection.
+        green.setSelected(true);
+// Create the Priority submenu.
+        Menu priorityMenu = new Menu("Priority");
+// Use radio menu items for the priority setting.
+// This lets the menu show which priority is used
+// and also ensures that one and only one priority
+// can be selected at any one time.
+        RadioMenuItem high = new RadioMenuItem("High");
+        RadioMenuItem low = new RadioMenuItem("Low");
+// Create a toggle group and use it for the radio menu items.
+        ToggleGroup tg = new ToggleGroup();
+        high.setToggleGroup(tg);
+        low.setToggleGroup(tg);
+// Select High priority for the default selection.
+        high.setSelected(true);
+// Add the radio menu items to the Priority menu and
+// add the Priority menu to the Options menu.
+        priorityMenu.getItems().addAll(high, low);
+        optionsMenu.getItems().add(priorityMenu);
+// Add a separator.
+        optionsMenu.getItems().add(new SeparatorMenuItem());
+// Create the Reset menu item.
+        MenuItem reset = new MenuItem("Reset");
+        optionsMenu.getItems().add(reset);
+// Add Options menu to the menu bar.
+        mb.getMenus().add(optionsMenu);
+        /*
+// Create the Options menu.
+        Menu optionsMenu = new Menu("_Options");
+// Create the Colors submenu.
+        Menu colorsMenu = new Menu("_Colors");
         MenuItem red = new MenuItem("Red");
         MenuItem green = new MenuItem("Green");
         MenuItem blue = new MenuItem("Blue");
@@ -56,14 +97,17 @@ public class MenuDemo extends Application {
         optionsMenu.getItems().add(priorityMenu);
 // Add a separator.
         optionsMenu.getItems().add(new SeparatorMenuItem());
+
 // Create the Reset menu item.
         MenuItem reset = new MenuItem("Reset");
         optionsMenu.getItems().add(reset);
 // Add Options menu to the menu bar.
         mb.getMenus().add(optionsMenu);
+ */
 // Create the Help menu.
         Menu helpMenu = new Menu("Help");
-        MenuItem about = new MenuItem("About");
+        ImageView aboutIV = new ImageView("AboutIcon.png");
+        MenuItem about = new MenuItem("About", aboutIV);
         helpMenu.getItems().add(about);
 // Add Help menu to the menu bar.
         mb.getMenus().add(helpMenu);
@@ -87,6 +131,14 @@ public class MenuDemo extends Application {
         low.setOnAction(MEHandler);
         reset.setOnAction(MEHandler);
         about.setOnAction(MEHandler);
+
+
+        // Add keyboard accelerators for the File menu.
+        open.setAccelerator(KeyCombination.keyCombination("shortcut+O"));
+        close.setAccelerator(KeyCombination.keyCombination("shortcut+C"));
+        save.setAccelerator(KeyCombination.keyCombination("shortcut+S"));
+        exit.setAccelerator(KeyCombination.keyCombination("shortcut+E"));
+
 // Add the menu bar to the top of the border pane and
 // the response label to the center position.
         rootNode.setTop(mb);
